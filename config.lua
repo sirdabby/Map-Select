481c1af442e252f71ac6facf8ba3a7bbe6e6cd6e
Dabby - Dev <115485826+sirdabby@users.noreply.github.com>
2024-04-15 17:05:23 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,18 @@
+-- Slide Dev - Discord.gg/slidedev - Made By Dabby
+
+Config = {}
+
+Config.OpenMenu = {
+    Command = "mapmenu",
+    Key = "K"
+}
+Config.Maps = {
+    ['big-bank'] = {
+        Coords = vector4(224.44, 218.43, 105.55, 168.36),
+        image = "https://cdn.discordapp.com/attachments/1212821251882491934/1229419775931846667/image.png?ex=662f9d64&is=661d2864&hm=8fc7f617c48ecec42b3d827153648e0ea8632914c1c2d7447367b1eb4afb8a8a&"
+    },
+    ['redgarage'] = {
+        Coords = vector4(-360.16, -828.4, 31.56, 330.66),
+        image = "https://cdn.discordapp.com/attachments/1212821251882491934/1229420059034652692/image.png?ex=662f9da7&is=661d28a7&hm=906cf84297ca36e1a49d35d9367429897e0cbb5ca2ffe6d54bf8774968073413&"
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'Slide Dev Map Selector'
+author 'Dabby'
+client_script { 'client/client.lua' }
+ui_page { 'html/ui.html' }
+shared_script { 'config.lua' }
+files { 'html/ui.html', 'html/main.css', 'html/app.js', 'html/*.*' }
